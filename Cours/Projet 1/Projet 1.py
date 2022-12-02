44646f054c7815a1ef142f0d2002f838ff97504c
Suboy <antoinee55dufil@gmail.com>
2022-12-02 09:09:37 +0100

    DB finished, no test

--- Cours/Projet 1/Projet 1.py
+++ Cours/Projet 1/Projet 1.py
@@ -30,13 +30,23 @@ def upload_img():
     res = res.fetchall()
     if form_pseudo in res:
         user_id = cur.execute(f'SELECT user_id FROM users WHERE user_name = {form_pseudo}')
+        user_id = user_id.fetchone()
         nb_pict = cur.execute(f'SELECT nb_pictures FROM users WHERE user_name = {form_pseudo}')
+        nb_pict = nb_pict.fetchone()
         db.commit()
-        file_name = f"{form_pseudo}{nb_pict}{file_slice(form_img.filename)}"
-        cur.execute(f"INSERT INTO photos (name, user_id) VALUES ({file_name}, {user_id})")
+        file_name = f"{form_pseudo}{nb_pict[0]}{file_slice(form_img.filename)}"
+        cur.execute(f"INSERT INTO photos (name, user_id) VALUES ({file_name}, {user_id[0]})")
+        cur.execute(f"UPDATE users SET nb_pictures = {nb_pict + 1} WHERE user_name = {form_pseudo}")
         db.commit()
     else:
-
+        cur.execute(f"INSERT INTO users (user_name, nb_pictures) VALUES ({form_pseudo}, '1')")
+        db.commit()
+        nb_pict = 1
+        file_name = f"{form_pseudo}{nb_pict}{file_slice(form_img.filename)}"
+        user_id = cur.execute(f'SELECT user_id FROM users WHERE user_name = {form_pseudo}')
+        db.commit()
+        user_id = user_id.fetchone()
+        cur.execute(f"INSERT INTO photos (name, user_id) VALUES ({file_name}, {user_id[0]})")
         file_name = ""
     form_img.save(f"{img_dir}{file_name}")
     return redirect("/")
